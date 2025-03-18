435827ceadd40e9c3429a0ec902f88992568bc46
Ansari <Saima.Ansari@saint-gobain.com>
2025-03-18 12:59:33 +0530

    2

--- main.tf
+++ main.tf
@@ -11,3 +11,11 @@ resource "azurerm_virtual_network" "vnet1" {
   resource_group_name = azurerm_resource_group.saima-rg.name
   
 }
+
+resource "azurerm_virtual_network" "vnet2" {
+  name = "vnet_saima2"
+  location = var.region
+  address_space = ["90.91.91.0/25"]
+  resource_group_name = azurerm_resource_group.saima-rg.name
+  
+}
