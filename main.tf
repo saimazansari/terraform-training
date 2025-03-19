985a2fc799c86074511077b2cfd13a7f3f02c4be
Saima Ansari <61002587+saimazansari@users.noreply.github.com>
2025-03-19 11:18:22 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,21 @@
+terraform {
+  cloud {
+    organization = "saintgobain"
+    ## Required for Terraform Enterprise; Defaults to app.terraform.io for Terraform Cloud
+    hostname = "app.terraform.io"
+
+    workspaces {
+      tags = ["terraformclouddemo"]
+    }
+  }
+}
+
 resource "azurerm_resource_group" "saima-rg" {
   name     = "saima"
   location = var.region
+tags = {
+     "owner" = "sg1"
+  }
 
 }
 
@@ -9,6 +24,9 @@ resource "azurerm_virtual_network" "vnet1" {
   location = var.region
   address_space = ["91.91.91.0/25"]
   resource_group_name = azurerm_resource_group.saima-rg.name
+tags = {
+     "owner" = "sg1"
+  }
   
 }
 
@@ -17,10 +35,9 @@ resource "azurerm_virtual_network" "vnet2" {
   location = var.region
   address_space = ["90.91.91.0/25"]
   resource_group_name = azurerm_resource_group.saima-rg.name
+tags = {
+     "owner" = "sg1"
+  }
   
 }
 
-tags = {
-Environment = "Prod"
-Owner = "Dev"
-}
